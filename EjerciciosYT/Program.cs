1df41f339586d5a320c4c6e9f9da3548515eeed2
BrahianL <brahianlopera323234@correo.itm.edu.co>
2025-12-09 17:39:56 -0500

    Ejercicios del 4 al 7

--- EjerciciosYT/Program.cs
+++ EjerciciosYT/Program.cs
@@ -9,7 +9,7 @@ namespace Ejercicios
         static void Main(string[] args)
         {
             Ejercicios obj = new Ejercicios();
-            obj.Ejercicio2();
+            obj.Ejercicio7();
 
         }
 
@@ -41,8 +41,97 @@ namespace Ejercicios
         }
 
 
+        // Pedir los valores de los catetos de un triángulo rectángulo y calcular la hipotenusa
+        public void Ejercicio3()
+        {
+
+            Console.WriteLine("Ingrese el valor del cateto 1 (en metros): ");
+            double cateto1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del cateto 2 (en metros): ");
+            double cateto2 = Convert.ToInt32(Console.ReadLine());
+            cateto1 = Math.Pow(cateto1, 2);
+            cateto2 = Math.Pow(cateto2, 2);
+            double hiponetusa = Math.Sqrt(cateto1 + cateto2);
+            Console.WriteLine("La hipotenusa es: " + hiponetusa);
+            Console.ReadKey();
+        }
 
 
+        // Pedir dos números y realizar las cuatro operaciones básicas (suma, resta, multiplicación y división)
+        public void Ejercicio4()
+        {
+            Console.WriteLine("Ingrese el valor del número 1: ");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor del número 2: ");
+            double num2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("La suma es: " + (num1 + num2));
+            Console.WriteLine("La resta es: " + (num1 - num2));
+            Console.WriteLine("La multiplicación es: " + (num1 * num2));
+            Console.WriteLine("La división es: " + (num1 / num2));
+            Console.ReadKey();
+        }
+
+
+        // Pedir una cantidad en grados Fahrenheit y convertirla a grados Celsius
+        public void Ejercicio5()
+        {
+            Console.WriteLine("Ingrese los grados Fahrenheit que quiere convertir a Celsius: ");
+            double Fahrenheit = Convert.ToInt32(Console.ReadLine());
+
+            double Celsius = (Fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Los grados Celsius son: " + Celsius);
+            Console.ReadKey();
+
+        }
+
+
+        // Pedir tres números y calcular la media aritmética
+        public void Ejercicio6()
+        {
+            Console.WriteLine("Ingrese él primer número: ");
+            double num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese él segundo número: ");
+            double num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese él tercer número: ");
+            double num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            double media = (num1 + num2 + num3) / 3;
+            Console.WriteLine("La media de los tres números es: " + media);
+            Console.ReadKey();
+        }
+
+
+        // Pedir una cantidad en minutos y convertirla a horas y minutos
+        public void Ejercicio7()
+        {
+
+            Console.WriteLine("Ingrese la cantidad de minutos: ");
+            int minutos = Convert.ToInt32(Console.ReadLine());
+            int horas = minutos / 60;
+            int Horasrestantes = minutos % 60;
+            Console.WriteLine("La cantidad de horas es: " + horas + " y los minutos restantes son: " + Horasrestantes);
+            Console.ReadKey();
+
+        }
+
+        public void Ejercicio8()
+        {
+
+
+        }
+
+        public void Ejercicio9()
+        {
+
+
+        }
+
+        public void Ejercicio10()
+        {
+
+
+        }
     }
 }
